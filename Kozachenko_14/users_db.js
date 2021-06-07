1cea456d9fdd7077c7d5efc10911744905bfff82
Illia <ilya.s.kozachenko@gmail.com>
2021-06-08 00:37:44 +0300

    home work 15 added

---
+++ Kozachenko_14/users_db.js
@@ -0,0 +1,28 @@
+'use strict'
+
+const users = [
+    {
+        id: 0,
+        name: "Boris",
+        age: 45,
+        depositAmount: 25000
+    },
+    {
+        id: 1,
+        name: "Frodo",
+        age: 64,
+        depositAmount: 1000000
+    },
+    {
+        id: 2,
+        name: "Mufasa",
+        age: 12,
+        depositAmount: 5000
+    },
+    {
+        id: 3,
+        name: "Trinity",
+        age: 28,
+        depositAmount: 15000
+    },
+];
